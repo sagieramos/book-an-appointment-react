e8c5b9f09910191f42e4575f756f9c1f4321a88f
nilab-khwaja <nilab.khwajazada1212@gmail.com>
2024-01-25 16:32:16 +0430

    update routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Vehicles from './components/Vehicles';
+import CarDetails from './components/CarDetails';
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<Vehicles />} />
+        <Route  path="/details/:id" element={<CarDetails />} />
+      </Routes>
     </div>
   );
 }
