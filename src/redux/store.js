b0870e741d50c018c80eac8fc5ba2bc39fd04189
Lawmsangi <ddarkphoenixx22@gmail.com>
2024-01-27 22:15:35 +0530

    Update store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import reservationsReducer from './reservationsSlice';
 import publicItemsReducer from './slices/publicItemsSlices';
 
 export const store = configureStore({
   reducer: {
-    reservationStore: reservationsReducer,
     items: publicItemsReducer, 
   },
 });
+export default store;
