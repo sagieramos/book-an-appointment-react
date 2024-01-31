c76e64bb59aa6ec422870ecade3c412b4efc6670
Lawmsangi <ddarkphoenixx22@gmail.com>
2024-01-31 22:39:23 +0530

    Update reservationDetails for styling

--- src/components/ReservationDetails.jsx
+++ src/components/ReservationDetails.jsx
@@ -3,7 +3,9 @@ import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
 import api from '../apiDomain.json';
-import car from '../assets/images/car.jpg';
+// import car from '../assets/images/car.jpg';
+// import car1 from '../assets/car1.jpeg';
+import style from './ReservationDetails.module.css';
 
 const ReservationDetails = () => {
   const { id } = useParams();
@@ -11,6 +13,23 @@ const ReservationDetails = () => {
   const [items, setItems] = useState([]);
   const { user } = useSelector((state) => state.profile);
   const navigate = useNavigate();
+
+  // I could not get data from api so I used mock data for styling
+
+  // const reservation = {
+  //   customer_id: 1,
+  //   reserve_for_use_date: '2021-09-01',
+  //   created_at: '2021-09-01',
+  // };
+  // const items = [
+  //   {
+  //     id: 1,
+  //     name: 'car',
+  //     description: 'car',
+  //     image_url: 'car.jpg',
+  //     show_reservation: 'show_reservation',
+  //   },
+  // ];
 
   useEffect(() => {
     const fetchReservationData = async () => {
@@ -43,13 +62,13 @@ const ReservationDetails = () => {
   }
 
   return (
-    <div>
+    <div className={style.resDetailsContainer}>
       <p>
-        Booked for:
+        Booked for:&nbsp;
         {reservation.reserve_for_use_date}
       </p>
       <p>
-        Created At:
+        Created at:&nbsp;
         {reservation.created_at}
       </p>
 
@@ -60,22 +79,22 @@ const ReservationDetails = () => {
             <img
               src={item.image_url ? `${api.apiDomain}/${item.image_url}` : car}
               alt={item.name}
-              style={{ width: '100px', height: '100px' }}
+              style={{ width: '200px', height: '200px' }}
             />
             <p>
-              Name:
+              Name:&nbsp;
               {item.name}
             </p>
             <p>
-              Description:
+              Description:&nbsp;
               {item.description}
             </p>
 
-            <button type="button" onClick={() => navigate(`/item/${item.id}`)}>About Car</button>
+            <button className={style['about-btn']} type="button" onClick={() => navigate(`/item/${item.id}`)}>About Car</button>
           </li>
         ))}
       </ul>
-      <button type="button" onClick={() => navigate(-1)}>Back</button>
+      <button className={style['back-btn']} type="button" onClick={() => navigate(-1)}>Back</button>
     </div>
   );
 };
