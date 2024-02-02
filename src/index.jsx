3f5b14a983442c0fe9a6e0228c451db68e2f100c
Stanley Osagie Ramos <sagiecyber@gmail.com>
2024-02-02 18:37:45 +0100

    Deploy

--- src/index.jsx
+++ src/index.jsx
@@ -4,13 +4,13 @@ import { Provider } from 'react-redux';
 import './index.css';
 import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
-import store from './redux/store';
+import reduxStore from './redux/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <Provider store={store}>
+  <Provider store={reduxStore}>
     <Router>
       <App />
     </Router>
